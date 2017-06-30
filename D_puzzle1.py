6b7b12fffb5c46bcaf9f100e51834f6e268f2030
Benjamin C. Jantzen <bjantzen@vt.edu>
2017-06-30 01:07:56 -0400

    Debugging D scriptswq.

--- D_puzzle1.py
+++ D_puzzle1.py
@@ -8,28 +8,33 @@ import time
 
 sense = SenseHat()
 
-print("Temperature" + 7*" " + "Humidity" + 7*" " + "Pressure" + 7*" " + " MagneticField\n\n")
+try:
 
-while True:
-    # measure variables
-    temperature = sense.get_temperature()
-    humidity = sense.get_humidity()
-    pressure = sense.get_pressure()
-    raw = sense.get_compass_raw()
-    m_field = raw['x']**2 + raw['y']**2 + raw['z']**2
-    
-    t = int(min([max([(temperature - 22.) * 255. / 15., 0]), 255]))
-    
-    color = [t, 0, 255 - t]
-    
-    t_out = str(temperature) + 5*" "
-    h_out = str(humidity) + 5*" "
-    p_out = str(pressure) + 5*" "
-    m_out = str(m_field) + 5*" "
+    print("Temperature" + 7*" " + "Humidity" + 7*" " + "Pressure" + 7*" " + " MagneticField\n\n")
+
+    while True:
+        # measure variables
+        temperature = sense.get_temperature()
+        humidity = sense.get_humidity()
+        pressure = sense.get_pressure()
+        raw = sense.get_compass_raw()
+        m_field = raw['x']**2 + raw['y']**2 + raw['z']**2
+        
+        t = int(min([max([(temperature - 27.) * 255. / 20., 0]), 255]))
+        
+        color = [t, 0, 255 - t]
+        
+        t_out = str(temperature) + 5*" "
+        h_out = str(humidity) + 5*" "
+        p_out = str(pressure) + 5*" "
+        m_out = str(m_field) + 5*" "
 
 
-    print(t_out + h_out + p_out + m_out, end='\r')
+        print(t_out + h_out + p_out + m_out, end='\r')
 
-    sense.set_pixels(64 * [color])
-    
-    time.sleep(0.1)
+        sense.set_pixels(64 * [color])
+        
+        time.sleep(0.1)
+
+except KeyBoardInterrupt:
+    sense.set_pixels(64 * [0,0,0])
